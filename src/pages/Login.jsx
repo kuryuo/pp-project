14f2b54a5812ff76b8f075463e8bf94634162e6d
ssftvyn <kutabare.wa@gmail.com>
2023-12-27 04:21:52 +0500

    auth + reg

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import '../styles/auth.css';
 import auth from '../images/auth.jpg';
 
@@ -6,24 +7,38 @@ function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(`Email: ${email}, Password: ${password}`);
+    try {
+      const response = await axios.post('http://192.168.122.49:8080/api/auth/authenticate', {
+        email,
+        password
+      });
+
+      localStorage.setItem('token', response.data.token);
+
+      axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.token}`;
+
+      console.log(`Email: ${email}, Password: ${password}`);
+      console.log(`Token: ${response.data.token}`); 
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
     <>
-      <div className="auth-container">
-        <img src={auth} alt="auth" className="auth-image" />
-        <form onSubmit={handleSubmit} className="auth-form">
-          <h1 className='auth-h1'>Войти</h1>
+      <div className='auth-container'>
+        <img src={auth} alt="auth" />
+        <form onSubmit={handleSubmit}>
+          <h1 className='auth-h1'>Вход</h1>
           <div className='label-container'>
           <label>
             Почта
             <input type="email" placeholder="Введите почту" value={email} onChange={(e) => setEmail(e.target.value)} />
           </label>
           <label>
-            Почта
+            Пароль
             <input type="password" placeholder="Введите пароль" value={password} onChange={(e) => setPassword(e.target.value)} />
           </label>
           </div>
--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,15 +1,30 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import '../styles/auth.css';
 import auth from '../images/auth.jpg';
 
 function Registration() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [profileType, setProfileType] = useState('');
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(`Email: ${email}, Password: ${password}, Profile Type: ${profileType}`);
+    try {
+      const response = await axios.post('http://192.168.122.49:8080/api/auth/register', {
+        email,
+        password
+      }
+    );
+
+      localStorage.setItem('token', response.data.token);
+
+      axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.token}`;
+
+      console.log(`Email: ${email}, Password: ${password}`);
+      console.log(`Token: ${response.data.token}`); 
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
@@ -27,13 +42,6 @@ function Registration() {
             Пароль
             <input type="password" placeholder="Введите пароль" value={password} onChange={(e) => setPassword(e.target.value)} />
           </label>
-          <label>
-            Тип профиля
-          </label>
-          <div className="button-container">
-            <button type="button" className="reg-button" onClick={() => setProfileType('Респондент')}>Респондент</button>
-            <button type="button" className="reg-button" onClick={() => setProfileType('Компания')}>Компания</button>
-          </div>
           </div>
           <button type="submit">Зарегистрироваться</button>
           <p className='p-auth'>Уже есть аккаунт?<a href="/login">Вход</a></p> 
